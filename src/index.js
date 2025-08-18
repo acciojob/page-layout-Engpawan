a26c70ef61387fd268683d0f12ca4ab25bbf0282
Pawan_Sharma <123291452+Engpawan@users.noreply.github.com>
2025-08-18 22:06:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ import App from "./components/App";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const first() =>{
+  return(
+    <div>
+    <h1>Welcome to my website</h1>
+  <h2>This is the content of my website.</h2>
+  <footer>©️ 2023 My Website. All rights reserved</footer>
+    </div>
+)
+}
+  root.render(first())
